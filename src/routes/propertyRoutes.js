54eb27d4de4159e8961bab854900ee5d82616a92
AxelVC22 <162675163+AxelVC22@users.noreply.github.com>
2026-01-03 16:55:48 -0600

    Fix: Refactor property routes to include additional endpoints

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const { createProperty } = require('../controllers/propertyController');
 const { verifyToken, authorizeRole } = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/uploadMiddleware');
 
-router.post('/', verifyToken, authorizeRole('Arrendador'), createProperty);
-//router.get('/', getProperties);
-// En tu archivo de rutas
+const {
+  createProperty,
+  getPropertyById,
+  updateProperty,
+  uploadPropertyImages,
+  deletePropertyImage,
+  getFilesByProperty,
+  getMyProperties,
+  searchProperties,
+} = require('../controllers/propertyController');
+
+router.get('/', searchProperties);
+router.get('/my-properties', verifyToken, authorizeRole(['Arrendador']), getMyProperties);
+router.get('/:id', getPropertyById);
+router.get('/:id/images', getFilesByProperty);
+router.post('/', verifyToken, authorizeRole(['Arrendador']), createProperty);
+router.put('/:id', verifyToken, authorizeRole(['Arrendador']), updateProperty);
 router.post(
-  '/properties/:id/images',
+  '/:id/images',
+  verifyToken,
+  authorizeRole(['Arrendador']),
   (req, res, next) => {
-    upload.array('imagenes', 5)(req, res, (err) => {
-      if (err instanceof multer.MulterError) {
-        return res.status(400).json({ success: false, message: `Error de carga: ${err.message}` });
-      } else if (err) {
+    upload.array('images', 10)(req, res, (err) => {
+      if (err) {
         return res.status(400).json({ success: false, message: err.message });
       }
       next();
     });
   },
   uploadPropertyImages,
+);
+router.delete(
+  '/:id/images/:imgId',
+  verifyToken,
+  authorizeRole(['Arrendador']),
+  deletePropertyImage,
 );
 
 module.exports = router;
