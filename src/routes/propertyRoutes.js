96aae61b604f15483fd76d6cefdd41f2c71e6f24
AxelVC22 <axlvaldez74@gmail.com>
2026-01-03 17:51:26 -0600

    feat: add recommended properties endpoint

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const { createProperty } = require('../controllers/propertyController');
 const { verifyToken, authorizeRole } = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/uploadMiddleware');
 
-router.post('/', verifyToken, authorizeRole('Arrendador'), createProperty);
-//router.get('/', getProperties);
-// En tu archivo de rutas
+const {
+  createProperty,
+  getPropertyById,
+  updateProperty,
+  uploadPropertyImages,
+  deletePropertyImage,
+  getFilesByProperty,
+  getMyProperties,
+  searchProperties,
+  getRecommendedProperties,
+} = require('../controllers/propertyController');
+
+router.get('/', searchProperties);
+router.get('/my-properties', verifyToken, authorizeRole(['Arrendador']), getMyProperties);
+router.get('/:id', getPropertyById);
+router.get('/:id/images', getFilesByProperty);
+router.post('/', verifyToken, authorizeRole(['Arrendador']), createProperty);
+router.put('/:id', verifyToken, authorizeRole(['Arrendador']), updateProperty);
 router.post(
-  '/properties/:id/images',
+  '/:id/images',
+  verifyToken,
+  authorizeRole(['Arrendador']),
   (req, res, next) => {
-    upload.array('imagenes', 5)(req, res, (err) => {
-      if (err instanceof multer.MulterError) {
-        return res.status(400).json({ success: false, message: `Error de carga: ${err.message}` });
-      } else if (err) {
+    upload.array('images', 10)(req, res, (err) => {
+      if (err) {
         return res.status(400).json({ success: false, message: err.message });
       }
       next();
@@ -20,5 +35,13 @@ router.post(
   },
   uploadPropertyImages,
 );
+router.delete(
+  '/:id/images/:imgId',
+  verifyToken,
+  authorizeRole(['Arrendador']),
+  deletePropertyImage,
+);
+
+router.get('/recommended', verifyToken, authorizeRole(['Cliente']), getRecommendedProperties);
 
 module.exports = router;
